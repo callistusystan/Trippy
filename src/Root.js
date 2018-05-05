616a061347eebc47f3f1c3f5c00d950b1456ef1f
callistusystan <callistusystan@gmail.com>
2018-05-05 12:56:01 +1000

    Install new dependencies

--- src/Root.js
+++ src/Root.js
@@ -1,6 +1,12 @@
 import React, { Component } from 'react';
+import firebase from 'firebase';
 
 class Root extends Component {
+    constructor(props) {
+        super(props);
+
+        
+    }
     render() {
         return (
             <div style={styles.container}>

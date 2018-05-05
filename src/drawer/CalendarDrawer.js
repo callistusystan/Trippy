f62188c7d0fb0ec8216382390b1ad99091b8df5a
callistusystan <callistusystan@gmail.com>
2018-05-05 17:12:22 +1000

    Changed backdrop for drawer

--- src/drawer/CalendarDrawer.js
+++ src/drawer/CalendarDrawer.js
@@ -5,7 +5,7 @@ class CalendarDrawer extends React.Component{
     render(){
         const {open,style} = this.props
         return(
-            <Drawer open={open} width={"100%"} containerStyle={{padding:70,...style}}>
+            <Drawer open={open} width={"100%"} containerStyle={{padding:70, boxShadow: undefined, ...style}}>
                 <div style={{background:"rgba(255,255,255,0.8)",width:"100%",height:"100%",padding:20}}>
                     <span style={{letterSpacing:1}}>SELECT TRAVELLING DATES</span>
                 </div>

700bf5c99580f5c53a24e5b113045b39307a7d09
David Copley <david.copley@monash.edu>
2018-05-05 22:04:43 +1000

    scroll stuff

--- src/drawer/FlightDrawer.js
+++ src/drawer/FlightDrawer.js
@@ -4,27 +4,36 @@ import {Card, Drawer, ListItem} from "material-ui"
 import Ranking from '../components/Ranking';
 
 const FlightCard = props => {
-    const {style,cardStyle, data} = props;
-    return(
-        <div style={{display:"flex",justifyContent:"center",alignItems:"center",...style}}>
+    const {style, cardStyle, data} = props;
+    return (
+        <div style={{display: "flex", justifyContent: "center", alignItems: "center", ...style}}>
             <Card
                 style={{
-                    boxShadow:"0 0 0 0",
-                    width:"98%",
-                    height:"95%",
-                    minWidth:300,
-                    padding:10,
+                    boxShadow: "0 0 0 0",
+                    width: "98%",
+                    height: "95%",
+                    minWidth: 300,
+                    padding: 10,
                     ...cardStyle
                 }}
             >
-                <span style={{letterSpacing:1}}>{data.airline}</span>
-                <img width={"100%"} src={data.airline_img} alt=""/>
+                <span style={{letterSpacing: 1}}>{data.airline}</span>
+                <div
+                    style={{
+                        height: 200,
+                        width: "100%",
+                        background: `url(${data.airline_img})`,
+                        backgroundSize: "contain",
+                        backgroundPosition:"center",
+                        backgroundRepeat:"no-repeat"
+                    }}
+                />
             </Card>
         </div>
     )
 }
 
-class FlightDrawer extends React.Component{
+class FlightDrawer extends React.Component {
 
     constructor(props) {
         super(props);
@@ -35,40 +44,46 @@ class FlightDrawer extends React.Component{
 
         const flightsRef = firebase.database().ref('flight_data');
         flightsRef.on('value', snapshot => {
-                this.setState({ flightItems: snapshot.val() });
-            });
+            this.setState({flightItems: snapshot.val()});
+        });
     }
 
     renderFlightItems = () => {
-        return this.state.flightItems.map(data => <FlightCard data={data} style={{flex:1}}/>);
+        return this.state.flightItems.map(data => <FlightCard data={data} style={{flex: 1}}/>);
     };
 
-    render(){
-        const {open,style} = this.props
-        return(
-            <Drawer open={open} width={"100%"} containerStyle={{padding:70, boxShadow: undefined, ...style}}>
-                <div style={{background:"rgba(255,255,255,0.8)",width:"100%",height:"100%",padding:20}}>
-                    <span style={{letterSpacing:1}}>SELECT FLIGHTS</span>
-                    <div style={{height:15}}/>
-                    <div style={{display:"flex",height: "95%",}}>
+    render() {
+        const {open, style} = this.props
+        return (
+            <Drawer open={open} width={"100%"} containerStyle={{padding: 70, boxShadow: undefined, ...style}}>
+                <div style={{background: "rgba(255,255,255,0.8)", width: "100%", height: "100%", padding: 20}}>
+                    <span style={{letterSpacing: 1}}>SELECT FLIGHTS</span>
+                    <div style={{height: 15}}/>
+                    <div style={{display: "flex", height: "95%",}}>
                         <div
+                            className={'flightDiv'}
                             style={{
-                                display:"flex",
-                                flexWrap:"wrap",
-                                overflowY:"scroll",
+                                display: "flex",
+                                flexWrap: "wrap",
+                                overflowY: "scroll",
                                 height: "100%",
                             }}
                         >
                             {this.renderFlightItems()}
-                            <span style={{flex:1,minWidth:300,}}/>
-                            <span style={{flex:1,minWidth:300,}}/>
-                            <span style={{flex:1,minWidth:300,}}/>
+                            <span style={{flex: 1, minWidth: 300,}}/>
+                            <span style={{flex: 1, minWidth: 300,}}/>
+                            <span style={{flex: 1, minWidth: 300,}}/>
                         </div>
                         <Ranking
                             path='abc123/flights'
                         />
                     </div>
                 </div>
+                <style>
+                    {`
+                        .flightDiv ::-webkit-scrollbar-button{ display: block; height: 13px; border-radius: 0px; background-color: #AAA; } ::-webkit-scrollbar-button:hover{ background-color: #AAA; } ::-webkit-scrollbar-thumb{ background-color: #a0a0a0; } ::-webkit-scrollbar-thumb:hover{ background-color: #CCC; } ::-webkit-scrollbar-track{ background-color: #efefef; } ::-webkit-scrollbar-track:hover{ background-color: #CCC; } ::-webkit-scrollbar{ width: 5px; }
+                    `}
+                </style>
             </Drawer>
         )
     }
--- src/maps/RootMap.js
+++ src/maps/RootMap.js
@@ -1,32 +1,46 @@
 import React, {Component} from 'react';
 import GoogleMapReact from "google-map-react";
-const AnyReactComponent = ({ text }) => <div style={{background:"red",width:100,height:100}}>{text}</div>;
-class RootMap extends Component{
-    state={
+
+const AnyReactComponent = ({text}) => <div style={{background: "red", width: 100, height: 100}}>{text}</div>;
+
+class RootMap extends Component {
+    state = {
         center: {
             lat: 0,
             lng: 0
         },
         zoom: 1
     }
-    render(){
-        const {center,zoom} = this.state
-        return(
-            <div style={{width:"100%",height:"100%"}}>
-            <GoogleMapReact
-                bootstrapURLKeys={{key: "AIzaSyB2KWm73tDpOV2yiIO7M7BoO-D_GIdNURM"}}
-                defaultCenter={center}
-                defaultZoom={zoom}
-                options={{
-                    disableDefaultUI: true
-                }}
-            >
-                {/*<AnyReactComponent*/}
+
+    zoominInterval = null
+
+    componentDidMount() {
+        this.zoominInterval = setInterval(() => this.setState({
+            center: {lng: -122.4192, lat: 37.7749},
+            zoom: this.state.zoom < 12 ? this.state.zoom + 0.25 : clearInterval(this.zoominInterval)
+        }), 150)
+    }
+
+    render() {
+        const {center, zoom} = this.state
+        return (
+            <div style={{width: "100%", height: "100%"}}>
+                <GoogleMapReact
+                    bootstrapURLKeys={{key: "AIzaSyB2KWm73tDpOV2yiIO7M7BoO-D_GIdNURM"}}
+                    // defaultCenter={center}
+                    center={center}
+                    defaultZoom={zoom}
+                    zoom={zoom}
+                    options={{
+                        disableDefaultUI: true
+                    }}
+                >
+                    {/*<AnyReactComponent*/}
                     {/*lat={47.608013}*/}
                     {/*lng={-122.335167}*/}
                     {/*text={'CAL'}*/}
-                {/*/>*/}
-            </GoogleMapReact>
+                    {/*/>*/}
+                </GoogleMapReact>
             </div>
         )
     }

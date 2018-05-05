e3abd6acc12b8e1f15a89708a6427b60ed79c52b
callistusystan <callistusystan@gmail.com>
2018-05-05 12:56:39 +1000

    setup firebase

--- src/Root.js
+++ src/Root.js
@@ -5,8 +5,17 @@ class Root extends Component {
     constructor(props) {
         super(props);
 
-        
+        const config = {
+            apiKey: 'AIzaSyBmrY2PFhtZU6rAstcTmrPpenLNBeInWiM',
+            authDomain: 'trippy-fbhack.firebaseapp.com',
+            databaseURL: 'https://trippy-fbhack.firebaseio.com',
+            projectId: 'trippy-fbhack',
+            storageBucket: '',
+            messagingSenderId: '230773547653'
+        };
+        firebase.initializeApp(config);
     }
+
     render() {
         return (
             <div style={styles.container}>

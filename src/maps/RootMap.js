58f3a4460f92d4a3241bfff26195fc6a0670ddc3
David Copley <david.copley@monash.edu>
2018-05-05 23:24:55 +1000

    spiky spike

--- src/maps/RootMap.js
+++ src/maps/RootMap.js
@@ -5,10 +5,7 @@ const AnyReactComponent = ({text}) => <div style={{background: "red", width: 100
 
 class RootMap extends Component {
     state = {
-        center: {
-            lat: 0,
-            lng: 0
-        },
+        center: {lng: -122.4192, lat: 37.7749},
         zoom: 1
     }
 
